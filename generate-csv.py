3f525d527857911504791fc62611bb7a4b578fd9
Daru13 <daru14@gmail.com>
2019-12-10 21:34:17 +0100

    Add language columns for URIs.
    Different heuristics are used to guess the language of the resource pointed by an URI (not always found).

--- generate-csv.py
+++ generate-csv.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+import re
 import os
 from urllib.parse import urlparse
 
@@ -38,7 +39,7 @@ def extract_path_title_into_serie(dataframe, column_name):
     path_titles = path_titles.rename(name)
     path_titles = path_titles.rename_axis(name)
 
-    return path_titles#
+    return path_titles
 
 resource_url_title_serie = extract_path_title_into_serie(parsed_resource_url_df, "resource_path")
 same_as_url_title_serie = extract_path_title_into_serie(parsed_same_as_url_df, "sameAs_path")
@@ -47,12 +48,57 @@ same_as_url_title_serie = extract_path_title_into_serie(parsed_same_as_url_df, "
 #print(same_as_url_title_serie)
 
 
+# Create dataframes containing the language of the ressource pointed by an URI (using a basic heuristic)
+languages_df = pd.read_csv("./data/language-codes.csv")
+
+codes_to_languages = {
+    **{code.lower(): lang for (code, lang) in zip(languages_df["code_2"], languages_df["language"])},
+    **{code.lower(): lang for (code, lang) in zip(languages_df["code_3"], languages_df["language"])}
+}
+
+codes_regex = re.compile("|".join([key for key in codes_to_languages.keys()]), re.IGNORECASE)
+
+def extract_language_into_serie(dataframe, col_prefix, output_col_name):
+    url_netlocs = dataframe[col_prefix + "netloc"]
+    url_paths = dataframe[col_prefix + "path"]
+
+    # Attempt to find a country code in netloc or path
+    languages = []
+
+    for netloc, path in zip(url_netlocs, url_paths):
+        split_path = path[1:].split("/")
+        if len(split_path) > 1:
+            # 1. Country code at the beginning of the path (if it contains at least two slashes)
+            match = re.fullmatch(codes_regex, split_path[0])
+            if match:
+                languages.append(codes_to_languages[match.group(0).lower()])
+                continue;
+
+        # 2. Country code at the beginning or at the end of the netloc (depending on its structure)
+        split_netloc = netloc.split(".")
+        string_to_search = split_netloc[0] if len(split_netloc) > 2 else split_netloc[-1]
+        
+        match = re.fullmatch(codes_regex, split_netloc[0])
+        languages.append(codes_to_languages[match.group(0).lower()] if match else "")
+    
+    url_languages = pd.Series(languages)
+    url_languages = url_languages.rename(output_col_name)
+    url_languages = url_languages.rename_axis(output_col_name)
+
+    return url_languages
+
+resource_url_language_serie = extract_language_into_serie(parsed_resource_url_df, "resource_", "resource_language")
+same_as_url_language_serie = extract_language_into_serie(parsed_same_as_url_df, "sameAs_", "sameAs_language")
+
+
 # Merge several dataframes into a single one
 final_df = all_df.join([
     parsed_resource_url_df,
     parsed_same_as_url_df,
     resource_url_title_serie,
-    same_as_url_title_serie
+    same_as_url_title_serie,
+    resource_url_language_serie,
+    same_as_url_language_serie
 ])  
 
 print(final_df)
