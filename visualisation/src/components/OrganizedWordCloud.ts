04f86f9e3c9e9e7ea9602f7d8b9f186d9ed5cc26
AChaffangeon <chaffangeon.adrien@gmail.com>
2020-02-03 10:55:24 +0100

    Update structure of project

---
+++ visualisation/src/components/OrganizedWordCloud.ts
@@ -0,0 +1,9 @@
+import { Column } from '../dataStructures/DataFrame';
+import * as d3 from "d3";
+import { ColumnAnalysis } from '../analyses/ColumnAnalysis';
+
+export class OrganizedWordCloud {
+    constructor(column: Column) {
+        
+    }
+}
