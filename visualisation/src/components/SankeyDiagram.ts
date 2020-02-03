1d8b763d171bcb3b8429ea2e4ccf787727d79b0a
AChaffangeon <chaffangeon.adrien@gmail.com>
2020-02-03 11:36:42 +0100

    Restore Sankey Diagram

--- visualisation/src/components/SankeyDiagram.ts
+++ visualisation/src/components/SankeyDiagram.ts
@@ -1,155 +1,202 @@
-import * as d3 from "d3";
 import { Column } from '../dataStructures/DataFrame';
-import { URLAnalysis } from '../analyses/URLAnalysis';
+import * as d3 from "d3";
+import { ColumnAnalysis } from '../analyses/ColumnAnalysis'
 
 const SPACE_BETWEEN_COLUMNS = 400;
 const SPACE_BETWEEN_ROWS = 50;
+const TEXT_FONT_SIZE = 60;
+
+type elementProperties = { height: number, fromX: number, fromY: number, toX: number, toY: number };
 
 export class SankeyDiagram {
-    column: Column;
     holder: d3.Selection<any, any, any, any>;
+
+    column: Column;
+    columnAnalysis: ColumnAnalysis;
+
+    skeleton: Map<string, elementProperties>;
+
+    properties: string[] = ["protocol", "subdomains", "domain", "tld", "path"];
 
     constructor(column: Column) {
         this.column = column
+        this.columnAnalysis = new ColumnAnalysis(this.column);
+        this.skeleton = new Map();
         this.holder = d3.select("body")
             .append("div")
-                .classed("sankey", true)
-                .append("svg");
+            .attr("id", "sankey-holder")
+            .classed("sankey", true)
+            .append("svg");
 
-        this.setupUI();
+        this.drawVertex();
+        this.drawEdges();
     }
 
-    setupUI() {
-        let skeleton: Record<string, Record<string, Record<string, number>>> = { protocol: {}, countryCode: {}, domain: {} };
-        let pairs: Record<string, Record<string, string[]>> = {};
-        let urls = [];
-        let urlanalysis;
+    private drawVertex() {
+        let values;
 
-        for (let value of this.column) {
-            if (validURL(value)){
-                urls.push(value);
-            }
-        }
-
-        for (let url of urls) {
-            urlanalysis = new URLAnalysis(url);
-            let keys = Object.keys(skeleton);
-            for (let i = 0; i < keys.length; i++) {
-                let urlProperty = urlanalysis.get(keys[i]);
-                if (!skeleton[keys[i]].hasOwnProperty(urlProperty)) {
-                    skeleton[keys[i]][urlProperty] = {height: 0, fromX: 0, fromY: 0, toX: 0, toY:0, width: 0}
-                }
-                skeleton[keys[i]][urlProperty]["height"] += 1;
-
-                if (i < keys.length - 1) {
-                    let nextUrlProperty = urlanalysis.get(keys[i + 1]);
-                    if (!pairs.hasOwnProperty(urlProperty)) {
-                        pairs[urlProperty] = {};
-                    }
-
-                    if (!pairs[urlProperty].hasOwnProperty(nextUrlProperty)) {
-                        pairs[urlProperty][nextUrlProperty] = [];
-                    }
-
-                    pairs[urlProperty][nextUrlProperty].push(url);
-                }
-            }
-        }
-
-        let x = 0; 
+        let x = 0;
         let y = 0;
-        let height = 0;
-        let columnHolder;
         let maxWidth = 0;
         let maxHeight = 0;
 
-        for (let param of Object.keys(skeleton)) {
+        let columnHolder: d3.Selection<SVGGElement, any, any, any>;
+        let text: d3.Selection<SVGTextElement, any, any, any>;
+        let rect: d3.Selection<SVGRectElement, any, any, any>;
+        let columnTitle: d3.Selection<SVGTextElement, any, any, any>;
+
+        for (let i = 0; i < this.properties.length; i++) {
+            columnHolder =
+                this.holder.append("g")
+                    .classed("sankey-column", true);
             y = 0;
-            columnHolder = this.holder.append("g");
-            maxWidth = 0;
 
-            for (let val of Object.keys(skeleton[param])) {
-                height = skeleton[param][val]["height"]
-                let text = columnHolder.append("text")
+            columnTitle =
+                this.holder.append("text")
+                    .attr("y", -SPACE_BETWEEN_ROWS)
+                    .style("font-size", `${TEXT_FONT_SIZE}px`)
+                    .text(this.properties[i]);
+
+            maxWidth = columnTitle.node().getBBox().width;
+
+            values = this.sortMap(this.columnAnalysis.getValues(this.properties[i]));
+            values.forEach(({ key: k, val: v }) => {
+                rect = columnHolder.append("rect")
                     .attr("x", x)
-                    .attr("y", y + height / 2)
-                    .text(val);
-                
-                let width = text.node().getComputedTextLength() + 10;
+                    .attr("y", y)
+                    .attr("height", v)
+                    .style("fill", this.getFillColor(v / this.column.length()));
 
-                maxWidth = Math.max(width, maxWidth);
+                text = columnHolder.append("text")
+                    .attr("y", y + v / 2)
+                    .style("fill", (v / this.column.length() < 0.3) ? "black" : "white")
+                    .style("font-size", `${Math.min(TEXT_FONT_SIZE * 2, Math.max(TEXT_FONT_SIZE / 2, 2 * TEXT_FONT_SIZE * v / this.column.length()))}px`)
+                    .style("alignment-baseline", "middle")
+                    .style("pointer-events", "none")
+                    .text(k);
 
-                skeleton[param][val]["fromX"] = x;
-                skeleton[param][val]["toX"] = x;
-                skeleton[param][val]["fromY"] = y;
-                skeleton[param][val]["toY"] = y;
+                this.addToolTipEvents(rect, v / this.column.length() * 100);
 
-                y += SPACE_BETWEEN_ROWS + height;
-            }
+                let width = text.node().getBBox().width;
+                maxWidth = (width > maxWidth) ? width : maxWidth;
 
-            maxHeight = Math.max(maxHeight, y - SPACE_BETWEEN_ROWS);
+                this.skeleton.set(k, { height: v, fromX: x, fromY: y, toX: x, toY: y });
+                y += v + SPACE_BETWEEN_ROWS;
+            });
 
-            for (let val of Object.keys(skeleton[param])) {
-                skeleton[param][val]["width"] = maxWidth;
-                skeleton[param][val]["fromX"] += maxWidth;
-                columnHolder.selectAll("text")
-                    .attr("x", (skeleton[param][val]["fromX"] + skeleton[param][val]["toX"])/2)
-                    .style("text-anchor", "middle");
-            }
-            x += SPACE_BETWEEN_COLUMNS + maxWidth;
+            y -= SPACE_BETWEEN_ROWS;
+            maxHeight = (maxHeight < y) ? y : maxHeight;
+
+            values.forEach(({ key: k, val: v }) => {
+                let eleProperty = this.skeleton.get(k);
+                eleProperty.fromX = eleProperty.toX + maxWidth;
+            });
+
+            columnTitle
+                .attr("x", x + maxWidth / 2)
+                .style("text-anchor", "middle");
+
+            columnHolder.selectAll("text")
+                .attr("x", x + maxWidth / 2)
+                .style("text-anchor", "middle");
+
+            columnHolder.selectAll("rect")
+                .attr("width", maxWidth);
+
+            x += maxWidth + SPACE_BETWEEN_COLUMNS;
         }
 
-        this.holder.attr("viewBox", `0,0,${x - SPACE_BETWEEN_COLUMNS},${maxHeight}`);
+        this.holder.selectAll(".sankey-column").each(function () {
+            let bbox = (this as SVGGElement).getBBox();
+            d3.select(this)
+                .selectAll()
+                .attr("transform", `translate(0,${maxHeight / 2 - bbox.height / 2})`)
+        });
 
-        let keys = Object.keys(skeleton);
-        for (let i = 0; i < keys.length - 1; i++) {
-            for(let value of Object.keys(skeleton[keys[i]])) {
-                for(let nextValue of Object.keys(pairs[value])){
-                    for(let url of pairs[value][nextValue]){
-                        let x1 = skeleton[keys[i]][value]["fromX"];
-                        let y1 = skeleton[keys[i]][value]["fromY"];
 
-                        let x2 = skeleton[keys[i + 1]][nextValue]["toX"];
-                        let y2 = skeleton[keys[i + 1]][nextValue]["toY"];
-                        let el = this.holder.append("path")
-                        .datum(url)
-                        .attr("d", `M${x1},${y1} L${x2},${y2}`)
-                        .style("stroke", "black")
-                        .node();
+        this.holder.attr("viewBox", `0, -${TEXT_FONT_SIZE + SPACE_BETWEEN_ROWS}, ${x - SPACE_BETWEEN_COLUMNS + TEXT_FONT_SIZE}, ${maxHeight + SPACE_BETWEEN_ROWS + TEXT_FONT_SIZE}`);
+    }
 
-                        el.addEventListener("mouseover", (e) => {
-                            d3.select("body")
-                            .append("div")
-                            .attr("id", `url-popup`)
-                            .style("position", "absolute")
-                            .style("top", `${e.pageY - 5}px`)
-                            .style("left", `${e.pageX + 10}px`)
-                            .style("background", "white")
-                            .text(url);
-                            d3.select(el).style("stroke", "red");
-                        })
+    private drawEdges() {
+        let values;
+        let map;
 
-                        el.addEventListener("mouseout", () => {
-                            d3.select(`#url-popup`).remove();
-                            d3.select(el).style("stroke", "black");
-                        })
-                        
-                        skeleton[keys[i]][value]["fromY"] += 1;
-                        skeleton[keys[i + 1]][nextValue]["toY"] += 1;
+        for (let i = 0; i < this.properties.length - 1; i++) {
+            values = this.sortMap(this.columnAnalysis.getValues(this.properties[i]));
+            values.forEach(({ key: firstValue, val: _ }) => {
+                map = this.columnAnalysis.pairs.get(firstValue)
+                this.sortMap(map).forEach(({ key: secondValue, val: nb }) => {
+                    if (!(this.skeleton.has(firstValue) && this.skeleton.has(secondValue))) {
+                        return;
                     }
-                }
-            }
+                    let ele = this.holder.append("path")
+                        .attr("d",
+                            `M${this.skeleton.get(firstValue).fromX},${this.skeleton.get(firstValue).fromY} 
+                            L${this.skeleton.get(secondValue).toX},${this.skeleton.get(secondValue).toY} 
+                            L${this.skeleton.get(secondValue).toX},${this.skeleton.get(secondValue).toY + nb - 1} 
+                            L${this.skeleton.get(firstValue).fromX},${this.skeleton.get(firstValue).fromY + nb - 1}`)
+                        .style("border", "none")
+                        .style("fill", this.getFillColor(nb / this.column.length()));
+
+                    this.addToolTipEvents(ele, nb / this.column.length() * 100);
+
+                    this.skeleton.get(firstValue).fromY += nb;
+                    this.skeleton.get(secondValue).toY += nb;
+                })
+            })
         }
     }
-}
 
-// Function from https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
-function validURL(str: string): boolean {
-    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-    return !!pattern.test(str);
+    private addToolTipEvents(element: d3.Selection<SVGElement, any, any, any>, percentage: number) {
+        element.node().addEventListener("mouseenter", (e) => {
+            let bbox = element.node().getBoundingClientRect();
+            let holderBBox = document.getElementById("sankey-holder").getBoundingClientRect();
+
+            let tooltip =
+                d3.select("#sankey-holder").append("div")
+                    .classed("tooltip", true)
+                    .style("position", "absolute")
+                    .style("pointer-events", "none")
+                    .style("background", "white")
+                    .text(percentage.toFixed(2) + "%");
+
+            let tooltipBBox = tooltip.node().getBoundingClientRect();
+            tooltip
+                .style("top", `${bbox.top + bbox.height / 2 - tooltipBBox.height / 2 - holderBBox.top}px`)
+                .style("left", `${bbox.left + bbox.width / 2 - tooltipBBox.width / 2 - holderBBox.left}px`)
+        })
+
+        element.node().addEventListener("mouseleave", (e) => {
+            d3.selectAll(".tooltip").remove();
+        })
+    }
+
+    private sortMap(map: Map<string, number>): { key: string, val: number }[] {
+        let temp: { key: string, val: number }[] = [];
+
+        map.forEach((v, k, _) => {
+            let i = 0;
+            while (i < temp.length && temp[i].val > v) {
+                i += 1;
+            }
+            temp.splice(i, 0, { key: k, val: v });
+        })
+
+        return temp; //temp.slice(0,5);
+    }
+
+    private svgTextBbox(text: string, fontSize: number) {
+        let ele = this.holder.append("text")
+            .style("font-size", `${fontSize}}px`)
+            .text(text);
+
+        let bbox = ele.node().getBBox();
+        ele.remove();
+
+        return bbox;
+    }
+
+    private getFillColor(percentage: number) {
+        return d3.interpolateBlues(percentage * 0.8 + 0.2);
+    }
 }
