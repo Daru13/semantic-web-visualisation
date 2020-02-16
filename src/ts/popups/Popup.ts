2402914d753257d6971f80c5238f17ca714942aa
Daru13 <daru14@gmail.com>
2020-02-16 17:33:25 +0100

    Prevent body scroll when a popup is open.

--- src/ts/popups/Popup.ts
+++ src/ts/popups/Popup.ts
@@ -1,15 +1,11 @@
-/** A class to create a pop up in the app */
-export class Popup{
-    /** The div classed as a pop up */
+export class Popup {
+    private static nbOpenPopups = 0;
+
     private holder: HTMLDivElement;
-    /** A mask apply behind the pop up to slightly hide the application */
     private maskBackground: HTMLDivElement;
-    /** The content div inside the pop up */
     readonly content: HTMLDivElement;
 
-    /** Title of the popup */
     private title: string;
-    /** Function called when the pop up is closing */
     private onClose: () => void;
 
     constructor(displayMask = false) {
@@ -19,21 +15,21 @@ export class Popup{
         this.content = document.createElement("div");
         this.content.classList.add("popup-content");
 
+        this.onClose = () => { };
+        this.title = "";
+
+        this.init(displayMask);
+    }
+
+    init(displayMask: boolean): void {
         if (displayMask) {
             this.initMask();
         }
         this.initUI();
-
-        this.onClose = () => { };
-        this.title = "";
+        this.display();
     }
 
-    /**
-     * Inits ui of the pop up with a title div, a close button and a content div
-     */
     initUI(): void {
-        let t = this;
-
         let titleBar = document.createElement("div");
         titleBar.classList.add("popup-titlebar");
         this.holder.appendChild(titleBar);
@@ -45,39 +41,33 @@ export class Popup{
 
         let closeButton = document.createElement("button");
         closeButton.classList.add("popup-close-button");
-        closeButton.addEventListener("click", () => { t.close(); } );
+        closeButton.addEventListener("click", () => { this.close(); } );
         closeButton.innerHTML = "Close";
         titleBar.appendChild(closeButton);
 
         this.holder.appendChild(this.content);
+    }
+
+    display(): void {
+        Popup.nbOpenPopups += 1;
+        document.body.classList.add("popup-displayed");
+
         document.body.appendChild(this.holder);
     }
 
-    /**
-     * Inits the background mask
-     */
     initMask(): void {
-        let t = this;
         this.maskBackground = document.createElement("div");
         this.maskBackground.classList.add("background-mask");
-        this.maskBackground.addEventListener("click", () => { t.close(); });
+        this.maskBackground.addEventListener("click", () => { this.close(); });
 
         document.body.appendChild(this.maskBackground);
     }
 
-    /**
-     * Sets the title of the pop up
-     * @param title title to set
-     */
     setTitle(title: string): void {
         this.title = title;
         this.holder.querySelector(".popup-title").innerHTML = this.title;
     }
 
-    /**
-     * Sets the max height for the content div
-     * @param maxHeight the max size
-     */
     setMaxHeightContent(maxHeight: number): void {
         let computedStyle = window.getComputedStyle(this.content, null);
         let paddingTop = parseInt(computedStyle.getPropertyValue("padding-top"));
@@ -86,7 +76,6 @@ export class Popup{
         this.content.style.maxHeight = `${maxHeight - paddingTop - paddingBottom}px`;
     }
 
-    /** Centers the popup in the window */
     center(): void {
         let popupBoundingBox = this.holder.getBoundingClientRect();
         let windowWidth = window.innerWidth;
@@ -99,26 +88,24 @@ export class Popup{
         this.holder.style.left = `${left}px`;
     }
 
-    /** Maximizes the popup */
     maximize(): void {
         this.holder.classList.add("maximized");
     }
 
-    /**
-     * Closes the popup and call onClose
-     */
     close(): void {
+        Popup.nbOpenPopups -= 1;
+        if (Popup.nbOpenPopups === 0) {
+            document.body.classList.remove("popup-displayed");
+        }
+
         if (this.maskBackground !== undefined) {
             this.maskBackground.remove();
         }
+
         this.holder.remove();
         this.onClose();
     }
 
-    /**
-     * Sets onClose
-     * @param onClose 
-     */
     setOnClose(onClose: () => void): void {
         this.onClose = onClose;
     }
